0f174bc7dff5a6fd900e17503bd8b6308e59f5a3
murad-97 <muradalshorman136@gmail.com>
2023-11-24 17:25:52 +0200

    murad

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -100,20 +100,12 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav">
-                        <a href="index.html" class="nav-item nav-link active">Home</a>
-                        <a href="about.html" class="nav-item nav-link">About</a>
-                        <a href="service.html" class="nav-item nav-link">Services</a>
-                        <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                            <div class="dropdown-menu bg-light m-0">
-                                <a href="feature.html" class="dropdown-item">Features</a>
-                                <a href="team.html" class="dropdown-item">Our Team</a>
-                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                                <a href="appoinment.html" class="dropdown-item">Appoinment</a>
-                                <a href="404.html" class="dropdown-item">404 Page</a>
-                            </div>
-                        </div>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        <a href={{ route("home") }} class="nav-item nav-link active">Home</a>
+                        <a href="about.html" class="nav-item nav-link">Jo Rover</a>
+                        <a href={{ route("events") }} class="nav-item nav-link">Events</a>
+                        <a href={{ route("team") }} class="nav-item nav-link">Team</a>
+                        <a href={{ route("roadmap") }} class="nav-item nav-link">Road Map</a>
+                        
                     </div>
                     <div class="ms-auto d-none d-lg-flex">
                         <a class="btn btn-sm-square btn-primary ms-2" href=""><i class="fab fa-facebook-f"></i></a>
